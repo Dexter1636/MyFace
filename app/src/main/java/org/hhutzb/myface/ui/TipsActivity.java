c84f92d5d804745dec3f1f9c74ac358a08a4d408
ZLQ <1102326984@qq.com>
2018-11-21 21:49:36 +0800

    'add back_arrow & tips icon'

--- app/src/main/java/org/hhutzb/myface/ui/TipsActivity.java
+++ app/src/main/java/org/hhutzb/myface/ui/TipsActivity.java
@@ -3,6 +3,8 @@ package org.hhutzb.myface.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.hhutzb.myface.R;
+
 public class TipsActivity extends AppCompatActivity {
 
     @Override
