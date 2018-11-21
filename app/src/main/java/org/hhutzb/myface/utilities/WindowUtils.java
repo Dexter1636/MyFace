850d2e84c0e1e6f97d8a106c28a412ca7b46bf83
Dexter <2779858398@qq.com>
2018-11-21 20:38:35 +0800

    DataActivity some changes

---
+++ app/src/main/java/org/hhutzb/myface/utilities/WindowUtils.java
@@ -0,0 +1,27 @@
+package org.hhutzb.emotion.utilities;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+
+
+public class WindowUtils {
+
+    /**
+     * 导航栏，状态栏透明
+     * @param activity
+     */
+    public static void setNavigationBarStatusBarTranslucent(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+}
